3e27687360f63ebd2edb2618b9c114d82e2281ac
Arian R <32659812+arianrah@users.noreply.github.com>
2019-10-09 19:34:29 -0700

    added new tweet scroll, tweet form hide, toggle error notes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,68 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(function() {
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" 
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+  $(".nav-new-tweet").click(function(){
+    $(".new-tweet").slideToggle(500);
+    $('.main-tweet-area').focus();
+  });
 
-let renderTweets = function(tweets) {
+$('.new-tweet-error').hide();
+
+$( "#tweet-form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  let tweetBody = $( this ).serialize();
+  if (!$('.main-tweet-area').val()) {
+    $(".new-tweet-error").html('Empty Tweet! 🐦😡').show().delay(2500).fadeOut();
+  }
+  if ($('.main-tweet-area').val().length > 140) {
+    $(".new-tweet-error").html('YOU HAVE SAID TOO MUCH 🤫').show().delay(2500).fadeOut();
+  } else {
+  $.ajax({
+    method: "POST",
+    url: "/tweets",
+    data: tweetBody
+  })
+    .then(loadTweets);
+}
+  });
+});
+  let loadTweets = () => {
+    $.get('/tweets', function(res){
+     renderTweets(res);
+    })
+};
+let renderTweets = (tweets) => {
+$('#tweet-container').empty();
 for (let tweet of tweets){
   let output = createTweetElement(tweet);
-  $(`#tweet-container`).append(output);
-  }
+  $(`#tweet-container`).prepend(output);
 }
-
+}
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 let createTweetElement = function(tweet) {
+  let date = new Date(tweet.created_at).toDateString();
+  
   let $tweets = (`
-  <article class='tweet'>
-  <header>
-    <img src='${tweet.user.avatars}'>  
-    <div>            
-      <h4 class='tweet-username'>${tweet.user.name}</h4>
-    <h4 class='tweet-userhandle'>${tweet.user.handle}</h4>
-  </div>
-  </header>
-  <span class='tweet-body'>${tweet.content.text}</span>
-  <footer>
-    <h4 class='tweet-post-date'>${tweet.created_at}/h4>
-    <div class='tweet-icons'>
-        <i class="fa fa-flag"></i>
-        <i class='fa fa-heart'></i>
-        <i class='fa fa-refresh'></i>
+    <article class='tweet'>
+    <header>
+      <img lass ='tweet-image' src='${ tweet.user.avatars }'>  
+      <div>            
+        <h4 class='tweet-username'>${ tweet.user.name }</h4>
+      <h4 class='tweet-userhandle'>${ tweet.user.handle }</h4>
     </div>
-  </footer>
-</article>
-`);
+    </header>
+    <span class='tweet-body'>${ escape(tweet.content.text) }</span>
+    <footer>
+      <h4 class='tweet-post-date'>${ date }</h4>
+      <div class='tweet-icons'>
+          <i class="fa fa-flag"></i>
+          <i class='fa fa-heart'></i>
+          <i class='fa fa-refresh'></i>
+      </div>
+    </footer>
+  </article>
+  `);
 return $tweets;
 };
-renderTweets(data);
-})
