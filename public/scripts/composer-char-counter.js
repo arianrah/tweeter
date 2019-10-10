5422a3455a96bb87f55b7e27b34c8ca141aa6ad6
Arian R <32659812+arianrah@users.noreply.github.com>
2019-10-10 12:50:55 -0700

    added stacked nav bar functionality for mobile users

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-		let maxChar = 140
 	$('.main-tweet-area').keyup(function() {
-        $('.counter').html((maxChar - $(this).val().length))
+        $('.counter').html((140 - $(this).val().length))
         $(this).val().length > 140 ? $('.counter').css("color", "red") : $('.counter').css("color", "#545149")
     })
 })
